a74b7c5f6ffb79d0ac8918c5e80e3bc47788eb79
ecthon <ecthon@gmail.com>
2023-01-23 16:31:54 -0400

    fix: remove unused tag

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,4 @@
-import { Box, Flex, Image, Link } from "@chakra-ui/react";
+import { Flex, Image, Link } from "@chakra-ui/react";
 import { NavLink } from 'react-router-dom'
 
 export function Header() {
