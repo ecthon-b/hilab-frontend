7c22d02a10ea5a6561ee05a48c5e31db21875d64
ecthon <ecthon@gmail.com>
2023-01-23 17:17:42 -0400

    feat[ShowList]: Modal

--- src/pages/ShowList/index.tsx
+++ src/pages/ShowList/index.tsx
@@ -1,4 +1,18 @@
-import { Box, Card, Image, Text, Wrap } from "@chakra-ui/react";
+import {
+    Box,
+    Card,
+    Image,
+    Text,
+    Wrap,
+    Modal,
+    ModalOverlay,
+    ModalContent,
+    ModalHeader,
+    ModalFooter,
+    ModalBody,
+    ModalCloseButton, 
+    useDisclosure,
+    Button} from "@chakra-ui/react";
 import { useEffect, useState } from "react";
 import { Header } from "../../components/Header";
 import { ICharacter } from "../../interfaces/character.interface";
@@ -6,6 +20,7 @@ import api from "../../services/api";
 
 export function ShowList() {
     const [characters, setCharacters] = useState<ICharacter[]>([]);
+    const { isOpen, onOpen, onClose } = useDisclosure()
 
     useEffect(() => {
         getCharacters();
@@ -17,6 +32,7 @@ export function ShowList() {
         console.log(data)
         setCharacters(data);
     }
+
     return (
         <Box
             bg='gray.100'
@@ -40,44 +56,64 @@ export function ShowList() {
                     align='center'
                     width='100%'
                 >
-                    {characters.map((character, index) => (
-                        <Card
-                            borderRadius='8px'
-                            key={index}
-                        >
-                            <Box
-                                width='280px'
-                                height='280px'
-                                flexDirection='column'
-                                borderRadius='8px 8px 0 0'
+                        {characters.map((character, index) => (
+                            <Card
+                                onClick={onOpen}
+                                borderRadius='8px'
+                                key={index}
+                                cursor='pointer'
                             >
-                                <Image
-                                    src={character.imageUrl}
-                                    boxSize='full'
-                                    objectFit='cover'
+                                <Box
+                                    width='280px'
+                                    height='280px'
+                                    flexDirection='column'
                                     borderRadius='8px 8px 0 0'
-                                />
-                            </Box>
-                            <Box
-                                height='74px'
-                                padding='16px 20px'
-                            >
-                                <Text
-                                    fontSize='12'
                                 >
-                                    NOME
-                                </Text>
-                                <Text
-                                    fontSize='1rem'
-                                    fontWeight='medium'
+                                    <Image
+                                        src={character.imageUrl}
+                                        boxSize='full'
+                                        objectFit='cover'
+                                        borderRadius='8px 8px 0 0'
+                                    />
+                                </Box>
+                                <Box
+                                    height='74px'
+                                    padding='16px 20px'
                                 >
-                                    {character.name}
-                                </Text>
-                            </Box>
-                        </Card>
-                    ))}
+                                    <Text
+                                        fontSize='12'
+                                    >
+                                        NOME
+                                    </Text>
+                                    <Text
+                                        fontSize='1rem'
+                                        fontWeight='medium'
+                                    >
+                                        {character.name}
+                                    </Text>
+                                </Box>
+                            </Card>
+                        ))}
                 </Wrap>
             </Box>
+
+            <Modal isOpen={isOpen} onClose={onClose}>
+                <ModalOverlay />
+                <ModalContent>
+                    <ModalHeader>Título</ModalHeader>
+                    <ModalCloseButton />
+                    <ModalBody>
+                        <Text>Body do modal</Text>
+                    </ModalBody>
+
+                    <ModalFooter>
+                        <Button colorScheme='blue' onClick={onClose}>
+                            Fechar
+                        </Button>
+                    </ModalFooter>
+                </ModalContent>
+
+            </Modal>
         </Box>
     )
 }
