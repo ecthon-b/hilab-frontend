bb3fc464ffa65766f98f255a19b52fc50a2d0728
ecthon <ecthon@gmail.com>
2023-01-24 21:07:58 -0400

    style[Header]: responsive

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,32 +2,35 @@ import { Flex, Image, Text } from "@chakra-ui/react";
 import { NavLink } from 'react-router-dom'
 
 export function Header() {
+
     return (
         <Flex
             bg='#040814'
             width='100%'
         >
             <Flex as='header'
+                id="header-size"
                 display='flex'
-                width='1216px'
-                height='100px'
+                width={{base: '90%', md:'90%', lg: '1216px'}}
+                height={{base: '80px', md:'90px', lg: '100px'}}
                 margin='0 auto'
                 justifyContent='space-between'
                 alignItems='center'
+                borderBottom='1px solid #1b202e'
                 bg='#040814'
             >
                 <Image
                     src="/img/disney.svg"
                     alt="Logomarca da Disney"
-                    width='150px'
+                    width={{ base: '70px', md:'100px', lg:'150px'}}
                 />
-
+                {}
                 <Flex as='nav'>
                     <NavLink to='/'>
                         <Text
                             padding='0 16px'
-                            fontSize='1.175rem'
-                            fontWeight='medium'
+                            fontSize={{base: '1rem', md:'1rem' ,lg:'1.175rem'}}
+                            fontWeight='normal'
                             textDecor='none'
                             color='#FFF'
                             transition=' color 0.3s'
@@ -37,7 +40,8 @@ export function Header() {
                         >
                             Home
                         </Text>
-                    </NavLink>                
+                    </NavLink>
+
                 </Flex>
             </Flex>
         </Flex>
